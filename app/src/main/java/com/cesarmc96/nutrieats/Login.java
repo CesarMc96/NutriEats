87de3204f38d0c72a8cea7bce3b4db9f75791c1b
César Alejandro Montaño Cortés <cesarin_mon@hotmail.com>
2019-06-04 03:08:05 -0500

    Arreglo 2.0

---
+++ app/src/main/java/com/cesarmc96/nutrieats/Login.java
@@ -0,0 +1,45 @@
+package com.cesarmc96.nutrieats;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText editaUsuario;
+    EditText editaContraseña;
+    Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editaUsuario = findViewById(R.id.editUsuario);
+        editaContraseña = findViewById(R.id.editContraseña);
+        btnEnviar = findViewById(R.id.btn);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = editaUsuario.getText().toString();
+                String contra = editaContraseña.getText().toString();
+
+                if (usuario.equals("admin") && contra.equals("123")){
+                    /*Toast toast1 = Toast.makeText(getApplicationContext(),"Entro", Toast.LENGTH_SHORT);
+                    toast1.show();*/
+
+                    Intent i = new Intent(getApplicationContext(), MenuPrincipal.class);
+                    startActivity(i);
+                } else {
+                    Toast toast1 = Toast.makeText(getApplicationContext(),"Error usuario/contraseña", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+            }
+        });
+    }
+}
