f266b11ac6c4eaea4d1488237a8474e92b45ac86
César Alejandro Montaño Cortés <cesarin_mon@hotmail.com>
2019-06-09 19:39:14 -0500

    Login Final. Modificacion de iconos.

---
+++ app/src/main/java/com/cesarmc96/nutrieats/ListaTarjetas.java
@@ -0,0 +1,13 @@
+package com.cesarmc96.nutrieats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaTarjetas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_tarjetas);
+    }
+}
